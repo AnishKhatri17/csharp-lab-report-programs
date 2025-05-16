cc8bb22c4bb8dc2cedf8e7adf9d80ecb8354fdee
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 22:18:48 +0545

    Sum and Product of two numbers in C# programming

---
+++ sum_and_product/Question_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum, product;
+            Console.WriteLine("Enter the value of First Number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of Second Number : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+            product = a * b;
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("The Sum of two numbers is :" + sum);
+            Console.WriteLine("The Product of two numbers is :" + product);
+            Console.ReadKey();  
+        }
+    }
+}
