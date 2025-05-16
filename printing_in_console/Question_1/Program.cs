c38ca8d55d9302fd4b4ac3bdf7a0a1bec3a07b3a
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 22:14:00 +0545

    Printing name and info in console simple program in C#

---
+++ printing_in_console/Question_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nMy name is Anish Khatri.");
+            Console.WriteLine("I am studying BCA.");
+            Console.WriteLine("I love to play Cricket.");
+
+            Console.ReadKey();
+        }
+    }
+}
