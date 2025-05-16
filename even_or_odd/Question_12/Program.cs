550e01cca0fa53393eff0326a9d6d18801dfdccf
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-16 22:42:43 +0545

    C# program to find even or odd number

---
+++ even_or_odd/Question_12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nC# Program to check whether a given number is even or odd.......");
+            int number;
+            Console.Write("\nEnter an integer number to check even or odd : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if(number%2 == 0)
+            {
+                Console.WriteLine("\n\nThe Given Number is Even.");
+            }
+            else
+            {
+                Console.WriteLine("\n\nThe Given Number is Odd.");
+            }
+                     Console.ReadKey();
+        }
+    }
+}
