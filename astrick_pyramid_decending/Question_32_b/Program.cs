815d577b05d402348a3625870b8781b3846013fb
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-17 08:01:44 +0545

    C# Program to take input number from user and display * pyramid in decreasing order

---
+++ astrick_pyramid_decending/Question_32_b/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_32_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nC# program to print the following pattern ......\n");
+            int i, j, rows;
+            Console.Write("\nEnter the number of rows (1-10) :");
+            rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            for(i = rows; i>=1; i--) 
+            {
+                for(j=1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+           }
+            Console.ReadKey();
+
+        }
+    }
+}
