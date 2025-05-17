6728b2353ced2b78959b71efaf97b3d48a7dc689
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-17 08:04:40 +0545

    C# program to take a number input from user to find whether the number is divisible by 5 but not by 7

---
+++ Divisibleby_5_not_by_7/Question_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nC# to display a number divisible by 5 but not by 11 .......\n");
+            int num;
+            Console.Write("\nEnter a integer number to check : ");
+            num = Convert.ToInt32(Console.ReadLine());
+                if (num %5==0 && num %11 !=0)
+            {
+                Console.WriteLine ("\n\nThe given number "+num+" is divisible by 5 but not by 11.");
+            }
+            else
+            {
+                Console.WriteLine("\n\nPlease enter another number !!");
+            }
+                Console.ReadKey();
+        }
+    }
+}
