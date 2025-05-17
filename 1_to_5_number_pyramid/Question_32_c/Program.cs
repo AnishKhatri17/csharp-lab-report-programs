766e3eb71b4f527ef22bd9b3be1097149786ac36
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-17 07:56:37 +0545

    Number Pyramid from 1 to 5 in incresing order

---
+++ 1_to_5_number_pyramid/Question_32_c/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_32_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nC# program to print the following pattern ........\n");
+            int i, j, rows;
+            Console.Write("\nEnter the number of rows (i.e 5) : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i=1; i<=rows; i++)
+            { 
+                for(j=1; j<=i; j++)
+                {
+                    Console.Write("\t"+j);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
