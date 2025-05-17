d1f027cc492d47b2a7c36914ede51740e8d0a75b
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-17 07:57:35 +0545

    C# Program to find the area and rectangle

---
+++ area_volume_of_rectangle/Question_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nProgram to find Area and Volume of a Rectangular Box ...");
+            int length, breadth, height, Area, Volume;
+            Console.WriteLine("\nEnter the Length of Rectangle.");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the Breadth of a Rectangle.");
+            breadth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the Height of a Rectangle.");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Area = length * breadth;
+            Volume = height * breadth *height;
+            Console.WriteLine("The area of a Rectangle is : "+Area);
+            Console.WriteLine("The volume of a Rectangle is : "+Volume);
+            Console.ReadKey();
+        }
+    }
+}
