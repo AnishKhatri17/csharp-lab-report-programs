ae4526ba614ed754b718df1ec3c437137c126272
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-17 08:06:04 +0545

    C# program to find the factorial of the given number

---
+++ factorial_of_given_number/Question_27/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nC# Program to find Factorial of a given number n .........");
+            int n,i,  fact=1;
+            Console.Write("\nEnter the number you want the factorial of : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            if(n == 0 && n<0)
+            {
+                Console.WriteLine("\n\n\nError!!! Factorial of a negative number doesn't exist ...");
+            }
+            else
+            {
+                for (i = 1; i <= n; i++)
+                {
+                    fact *= i;
+
+                }
+                Console.WriteLine("\n\n\nThe factorial of the given number " + n + " is : " + fact);
+            }
+            Console.ReadKey();
+        }
+                      
+    }
+}
