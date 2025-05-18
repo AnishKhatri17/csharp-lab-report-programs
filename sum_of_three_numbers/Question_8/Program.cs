35d676f9810c35d2d9f60ce88ff638a6e67df7bd
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:45:11 +0545

    C# program to display the sum of three given numbers

---
+++ sum_of_three_numbers/Question_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nProgram to perform Arithmetic Operations .....");
+            int num1, num2, num3, sum;
+            Console.Write("Enter the First Number : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Second Number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Third Number : ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            sum = num1 + num2 + num3;
+            Console.WriteLine("\n\n\nThe sum of Three Number is : "+ sum);
+            Console.ReadKey();      
+
+
+        }
+    }
+}
