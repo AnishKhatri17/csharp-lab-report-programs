23ceaf154597b465fca73595ed12f30225c59eb5
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:43:31 +0545

    C# program to display the sum of given array elements

---
+++ sum_of_array_elements/Question_33/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nC# program to find sum of 20 numbers is an Array ............");
+            // Array Declaration ...
+            int[] a = new int[20];
+            int i, n, sum = 0;
+            Console.Write("\nEnter the no. of int array elements (max 20) : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter the Array Elemets : \n");
+            // Enter the array elements, taking inputs from the user..
+            for(i=0; i<n; i ++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Calculating the sum .....
+            for(i=0; i<n; i++)
+            {
+                sum = sum + a[i];   
+            }
+            Console.WriteLine("\n\n\nThe sum of the Array Elements are : " +sum);
+            Console.ReadKey();
+        }
+    }
+}
