7fcb0c73df6b45f7c8757f208df88cb768368f63
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:39:21 +0545

    C# program to generate area of a circle based on given radius

---
+++ radius_of_circle/Question_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\nEnter the Radius of a Circle : \t");
+
+            double r = Convert.ToDouble(Console.ReadLine());
+            //float r = Convert.ToSingle(Console.ReadLine());
+            double area = Math.PI * r * r;
+            Console.WriteLine("\nThe area of a Circle is : " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
