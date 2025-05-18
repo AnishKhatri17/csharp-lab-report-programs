f2b469d9c865043d0592e04385c14fa21409c149
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:37:01 +0545

    C# Program to display prime numbers from 1 to 100

---
+++ prime_num_1_to_100/Question_30/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Program to find and display all prime numbers from 1 to 100...");
+
+            for (int num = 2; num <= 100; num++) // Start from 2 since 1 isn't prime number ....
+            {
+                bool isPrime = true;
+
+                for (int i = 2; i * i <= num; i++) // Check divisibility up to sqrt(num)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                        break; // No need to check further ....
+                    }
+                }
+
+                if (isPrime)
+                    Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\n\nPrime numbers printed successfully!");
+            Console.ReadLine();
+        }
+        
+    }
+}
