d774ea448d4353c096911b9f08a7aa1bcf77e281
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:44:11 +0545

    C# program to display the sum of given n numbers by the user.

---
+++ sum_of_n_numbers/Question_26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, n;
+            Console.WriteLine("\n\nC# Program to display sum of numbers between 1 to 50 .......");
+
+            Console.Write("\nEnter the max number you want the sum of : ");
+            n = int.Parse(Console.ReadLine());
+            for(int i=1; i<=n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("\n\n\nThe sum of numbers from 1 to 50 is : "+sum) ;
+            Console.ReadKey();
+        }
+    }
+}
