820b3b34ed4e705615f4f36aa438873e23ae1707
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:35:06 +0545

    C# program that takes input number ffrom user and if user gives 5, it will geneerate multiplication table upto 5

---
+++ multiplication_table_n_number/Question_29/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nMultiplication Table upto N terms : ");
+            int n, m, i;
+            Console.Write("\n\nEnter the number you want the Multiplication Table of : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0 && n < 0)
+            {
+                Console.WriteLine("\n\nSorry! Negative numbers and 0 don't have multiplication table !!!!!.");
+            }
+            else
+            {
+                Console.WriteLine("\n\n\nThe multiplication table of "+n+" is : \n");
+                for (i = 1; i <= 10; i++)
+                {
+                    m = n * i;
+                    Console.WriteLine("\t" + n + " * " + i + " \t= " +m);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
