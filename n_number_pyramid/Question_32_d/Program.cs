966b64475c4f4db3c9d66d256188189598f5d377
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:36:17 +0545

    C# program to generate number pyramid based on the user input number

---
+++ n_number_pyramid/Question_32_d/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_32_d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nC# program to print the following ................\n");
+            int i, j, rows;
+            Console.Write("\nEnter the number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for(i=1; i<=rows; i++)
+            {
+                for(j=1; j<=i;j++)
+                {
+                    Console.Write("\t"+i);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
