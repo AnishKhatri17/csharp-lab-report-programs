d02895e8364d4ae661ee02a76613ba5dda33714c
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-05-18 10:38:22 +0545

    C# program to find profit or loss based on the given cost price and sales price

---
+++ profit_or_loss/Question_17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nC# program to find out Profit or Loss Amount ......");
+            int cp, sp, profit, loss;
+            Console.Write("\n\nEnter the Cost Price of the Product : ");
+            cp = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Sales Price of the Product : ");
+            sp = Convert.ToInt32(Console.ReadLine());
+
+                  if(sp > cp)
+            {
+                Console.WriteLine("\n\n\nCongratulations !! You have gained Profit .");
+                profit = sp - cp;
+                Console.WriteLine("\nThe profit Amount is :"+profit);
+            }
+                  else 
+            { 
+                    Console.WriteLine("\n\n\nNooooo !! You have beared Loss .");
+                loss = cp - sp;
+                Console.WriteLine("\nThe loss Amount is :"+loss);
+            }
+                        Console.ReadKey();
+        }
+    }
+}
